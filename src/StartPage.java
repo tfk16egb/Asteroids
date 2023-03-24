e701c7bed81ac6ff2303d36477c019a5dcbfebf3
Emil Berman <tfk16egb@hig.se>
2023-03-24 12:55:32 +0100

    Alla bilder ligger med Bara mainmenu som inte vill vara med i PLAY-state

--- src/StartPage.java
+++ src/StartPage.java
@@ -1,13 +1,12 @@
+import Factory.BackgroundImageFactory;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.ResourceBundle;
+
 
 public class StartPage extends Application {
     public static int WIDTH = 600;
@@ -22,51 +21,51 @@ public class StartPage extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        StackPane root = new StackPane();
-        VBox layout2 = new VBox();
-
-        //root.setAlignment(Pos.CENTER);
-        //layout2.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
-        BackgroundImage bImg = new BackgroundImage(
-                startImg,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.CENTER,
-                new BackgroundSize(WIDTH, HEIGHT, true, true, true, true)
-        );
-        Background startBImg = new Background(bImg);
-        root.setBackground(startBImg);
-
-        Scene scene2 = new Scene(layout2, WIDTH, HEIGHT);
-        BackgroundImage bImg0 = new BackgroundImage(
-                playImg,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.CENTER,
-                new BackgroundSize(WIDTH, HEIGHT, true, true, true, true)
-        );
-        Background playBImg = new Background(bImg0);
-        layout2.setBackground(playBImg);
+        BackgroundImageFactory bImgFactory = new BackgroundImageFactory(WIDTH, HEIGHT);
+        bImgFactory.loadImages(
+                "playgame.png",
+                "startscreen.png",
+                "scoreboard.png");
 
         Button startBtn = new Button("START");
-        startBtn.setTranslateY(-45);
-        startBtn.setOnAction(e -> primaryStage.setScene(scene2));
-
-        Button backwards = new Button("Backwards");
-        backwards.setTranslateY(10);
-        backwards.setTranslateX(10);
-        backwards.setOnAction(e -> primaryStage.setScene(scene));
+        Button playGameBtn = new Button("PLAY");
+        playGameBtn.setTranslateY(20);
+        Button mainMenuBtn = new Button("MAIN MENU");
+        Button scoreBtn = new Button("SCOREBOARD");
+        scoreBtn.setTranslateY(20);
 
         TextField text = new TextField();
         text.setMaxWidth(100);
 
-        root.getChildren().addAll(startBtn);
-        layout2.getChildren().addAll(backwards, text);
+
+        StackPane mainPane = new StackPane();
+        StackPane playPane = new StackPane();
+        StackPane scorePane = new StackPane();
+
+
+        //Main menu setup
+        Scene mainScene = new Scene(mainPane, WIDTH, HEIGHT);
+        mainPane.setBackground(bImgFactory.getBackground("startscreen.png"));
+
+        //Insert name and play menu setup
+        Scene playScene = new Scene(playPane, WIDTH, HEIGHT);
+        playPane.setBackground(bImgFactory.getBackground("playgame.png"));
+
+        //Scoreboard menu setup
+        Scene scoreScene = new Scene(scorePane,WIDTH, HEIGHT);
+        scorePane.setBackground(bImgFactory.getBackground("scoreboard.png"));
+
+
+        startBtn.setOnAction(e -> primaryStage.setScene(playScene));
+        mainMenuBtn.setOnAction(e -> primaryStage.setScene(mainScene));
+        scoreBtn.setOnAction(e -> primaryStage.setScene((scoreScene)));
+
+        mainPane.getChildren().addAll(startBtn, scoreBtn);
+        playPane.getChildren().addAll(mainMenuBtn, text, playGameBtn);
+        scorePane.getChildren().add(mainMenuBtn);
 
         primaryStage.setTitle("Asteroid Shooter");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
 }
